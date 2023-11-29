5784aea9a7fe8144fca568832bf1a8d41238a2ed
Omar_Ameer <omar.ameer244@gmail.com>
2023-11-29 22:42:35 +0200

    Payment Details View Done.

--- lib/Features/checkout/presentation/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/widgets/payment_details_view_body.dart
@@ -1,10 +1,20 @@
+import 'dart:developer';
+
 import 'package:credit_card_checkout_app/Features/checkout/presentation/widgets/custom_credit_card.dart';
 import 'package:credit_card_checkout_app/Features/checkout/presentation/widgets/custom_main_button.dart';
 import 'package:credit_card_checkout_app/Features/checkout/presentation/widgets/payment_methods_list_view.dart';
 import 'package:flutter/material.dart';
 
-class PaymentDetailsViewBody extends StatelessWidget {
+class PaymentDetailsViewBody extends StatefulWidget {
   const PaymentDetailsViewBody({super.key});
+
+  @override
+  State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
+}
+
+class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +36,15 @@ class PaymentDetailsViewBody extends StatelessWidget {
             ),
           ),
         ),
-        const SliverToBoxAdapter(
+        SliverToBoxAdapter(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
-                CustomCreditCard(),
+                CustomCreditCard(
+                  formKey: formKey,
+                  autovalidateMode: autovalidateMode,
+                ),
               ],
             ),
           ),
@@ -44,7 +57,15 @@ class PaymentDetailsViewBody extends StatelessWidget {
               alignment: Alignment.bottomCenter,
               child: CustomMainButton(
                 text: "Pay",
-                onPressed: () {},
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    log('Payment Succesful');
+                    formKey.currentState!.save();
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
               ),
             ),
           ),
